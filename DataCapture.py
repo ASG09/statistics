1cde9fb28ba17277863479111ac4c702bbc9a266
asg09 <andre.senne@mandaum.com>
2022-10-22 13:57:21 -0300

    move method inside Stats class
        - it makes more sense to be here than on DataCapture

--- DataCapture.py
+++ DataCapture.py
@@ -2,6 +2,15 @@ class Stats:
 
     def __init__(self, data: dict) -> None:
         self.data = data
+        for n in self.data['values']:
+            self.data['less'] = {i: ((self.data['less'][i] + 1) if n < i else (self.data['less'][i])) for i in range(1001)}
+            self.data['greater'] = {i: ((self.data['greater'][i] + 1) if n > i else (self.data['greater'][i])) for i in range(1001)}
+
+            self.data['between'] = {
+                i: {
+                    j: ((self.data['between'][i][j] + 1) if (n >= i and n <= j) else (self.data['between'][i][j])) for j in range(1001)
+                } for i in range(1001)
+            }
 
     def less(self, n: int) -> int:
         '''returns the amount of numbers added less than a given number n'''
@@ -28,8 +37,8 @@ class Stats:
 class DataCapture:
 
     def __init__(self) -> None:
-        self.values = []
         self.data = {}
+        self.data['values'] = []
         self.data['less'] = {i: 0 for i in range(1001)}
         self.data['between'] = {i: {j: 0 for j in range(1001)} for i in range(1001)}
         self.data['greater'] = {i: 0 for i in range(1001)}
@@ -39,19 +48,10 @@ class DataCapture:
 
         if not isinstance(n, int):
             raise TypeError('n must be an int')
-        self.values.append(n)
+        self.data['values'].append(n)
         
-
     def build_stats(self) -> Stats:
         '''creates a stats instance'''
 
-        for n in self.values:
-            self.data['less'] = {i: ((self.data['less'][i] + 1) if n < i else (self.data['less'][i])) for i in range(1001)}
-            self.data['greater'] = {i: ((self.data['greater'][i] + 1) if n > i else (self.data['greater'][i])) for i in range(1001)}
-            self.data['between'] = {
-                i: {
-                    j: ((self.data['between'][i][j] + 1) if (n >= i and n <= j) else (self.data['between'][i][j])) for j in range(1001)
-                } for i in range(1001)
-            }
         stats = Stats(self.data)
         return stats
