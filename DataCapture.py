af3f9416601185db94f2490e448eeb3d10dfb27a
asg09 <andre.senne@mandaum.com>
2022-10-25 21:11:42 -0300

    improving variable names

--- DataCapture.py
+++ DataCapture.py
@@ -1,24 +1,24 @@
 class Stats:
 
-    def __init__(self, data: dict) -> None:
+    def __init__(self, data: list) -> None:
         '''Builds the possible statistics results and returns a stats instance
         
         Keyword arguments:
-        data -- a dictionary with a list called "values", containing the numbers to be queried
+        data -- a list containing the numbers to be queried
         '''
-
         self.data = data
-        for n in self.data['values']:
-            self.data['less'] = {
+        self.numbersLowerThan = {}
+        for n in self.data:
+            self.numbersLowerThan = {
                 i: (
                     (
-                        1 if ('less' not in self.data or self.data['less'][i] == None)
-                        else (self.data['less'][i] + 1)
+                        1 if (i not in self.numbersLowerThan or self.numbersLowerThan[i] == None)
+                        else (self.numbersLowerThan[i] + 1)
                     ) if n < i
                     else
                     (
-                        0 if ('less' not in self.data or self.data['less'][i] == None)
-                        else self.data['less'][i]
+                        0 if (i not in self.numbersLowerThan or self.numbersLowerThan[i] == None)
+                        else self.numbersLowerThan[i]
                     )
                 ) for i in range(1001)
             }
@@ -29,10 +29,9 @@ class Stats:
         Keyword arguments:
         n -- the number to be queried
         '''
-
         if not isinstance(n, int):
             raise TypeError('n must be an int')
-        return self.data['less'][n]
+        return self.numbersLowerThan[n]
 
     def between(self, n: int, m: int) -> int:
         '''Compare integers n and m with the data and returns the amount of numbers between them (inclusive).
@@ -41,7 +40,6 @@ class Stats:
         n -- the lower number to be queried
         m -- the greater number to be queried
         '''
-
         if (not isinstance(n, int) or not isinstance(m, int)):
             raise TypeError('n and m must be int')
 
@@ -53,17 +51,15 @@ class Stats:
         Keyword arguments:
         n -- the number to be queried
         '''
-
         if not isinstance(n, int):
             raise TypeError('n must be an int')
-        return len(self.data['values']) - self.less(n+1)
+        return len(self.data) - self.numbersLowerThan[n+1]
 
 
 class DataCapture:
 
     def __init__(self) -> None:
-        self.data = {}
-        self.data['values'] = []
+        self.data = []
 
     def add(self, n: int) -> None:
         '''Adds an int to be included on the statistic calculations. Returns None
@@ -71,13 +67,11 @@ class DataCapture:
         Keyword arguments:
         n -- the number to be added
         '''
-
         if not isinstance(n, int):
             raise TypeError('n must be an int')
-        self.data['values'].append(n)
+        self.data.append(n)
 
     def build_stats(self) -> Stats:
         '''Builds the possible statistics results and returns a stats instance, to generate the queries'''
-
         stats = Stats(self.data)
         return stats
